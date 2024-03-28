1169d4c81705a34ca2691177daeba14e52dfb448
Aki Van Ness <aki@lethalbit.net>
2024-03-28 09:40:15 -0700

    actions: search: some work on search stuff, currently annoyed with how meilisearch deals with the matches even on conjoined phrases splitting them into N matches even though it should only be one

--- bookwurm/actions/search.py
+++ bookwurm/actions/search.py
@@ -1,10 +1,12 @@
 # SPDX-License-Identifier: BSD-3-Clause
+import json
 import logging          as log
 from argparse           import ArgumentParser, Namespace
 from pathlib            import Path
-import json
+from io                 import StringIO, SEEK_SET, SEEK_END
 
-from rich.console       import Console
+from rich               import print
+from rich.panel         import Panel
 
 from meilisearch        import Client
 from meilisearch.index  import Index
@@ -24,6 +26,12 @@ class SearchAction(BookwurmAction):
 
 	def register_args(self, parser: ArgumentParser) -> None:
 		parser.add_argument(
+			'--detailed-results', '-D',
+			action = 'store_true',
+			help   = 'Enable detailed results'
+		)
+
+		parser.add_argument(
 			'--limit', '-l',
 			type    = int,
 			default = 20,
@@ -35,12 +43,75 @@ class SearchAction(BookwurmAction):
 			type = str
 		)
 
+	def _print_detailed_results(self, results: dict, query: str, limit: int, idx: Index):
+		CONTEXT_DELTA = 128
+		for hit in results['hits']:
+			doc_id      = hit['id']
+			doc_path    = Path(json.loads(hit['file']))
+			doc_meta    = f'[red]{hit["type"].upper()}[/], [yellow]{hit["total_pages"]} pages[/]'
+			doc_matches = hit['_matchesPosition']
+
+			print(f' [green]*[/] [cyan]{hit["title"]}[/] ([magenta]{len(doc_matches.keys())} matches[/])')
+			print(f'   [link=file://{doc_path}][blue]{doc_path.stem}[/][/] ({doc_meta})')
+
+			for page, matches in doc_matches.items():
+				d = idx.get_document(doc_id, {
+					'fields': [ page ]
+				})
+
+				if page.startswith('pages.'):
+					page_idx = page.split('.')[-1]
+					p = d.pages[page_idx]
+					ibuff = StringIO(p)
+					obuff = StringIO()
+
+					obuff.seek(0, SEEK_SET)
+					obuff.write('[dim]')
+
+					curr_pos = 0
+					for match in matches:
+						start_pos = match['start']
+						end_pos   = match['start'] + match['length']
+						curr_pos  = start_pos - CONTEXT_DELTA
+						if curr_pos < 0:
+							curr_pos = start_pos
+
+						ibuff.seek(curr_pos, SEEK_SET)
+						obuff.write(ibuff.read(start_pos - curr_pos))
+						obuff.write('[/][bold magenta]')
+						obuff.write(ibuff.read(match['length']))
+						obuff.write('[/][dim]')
+						obuff.write(ibuff.read(end_pos + CONTEXT_DELTA))
+						curr_pos = end_pos + CONTEXT_DELTA
+
+
+					obuff.write('[/]')
+					res = obuff.getvalue()
+					obuff.close()
+					ibuff.close()
+
+
+					print(Panel(
+						res.encode('utf-8').decode('unicode_escape').strip(),
+						title = f'Page [cyan]{page_idx}[/] [dim magenta]({len(matches)} matches)[/]'
+					))
+
+
+
+
+
+	def _print_simple_results(self, results: dict, query: str, limit: int):
+		for hit in results['hits']:
+			doc_path = Path(json.loads(hit['file']))
+			doc_meta = f'[red]{hit["type"].upper()}[/], [yellow]{hit["total_pages"]} pages[/]'
+
+			print(f' [green]*[/] [cyan]{hit["title"]}[/] ([magenta]{len(hit["_matchesPosition"].keys())} matches[/])')
+			print(f'   [link=file://{doc_path}][blue]{doc_path.stem}[/][/] ({doc_meta})')
+
 	def run(self, args: Namespace, config: dict) -> int:
 		query: str = args.query
 		limit: int = args.limit
-
-		console = Console()
-		print = console.print
+		detailed: bool = args.detailed_results
 
 		print(f'Searching for: \'{query}\'')
 
@@ -55,28 +126,26 @@ class SearchAction(BookwurmAction):
 			log.error(f'Unable to query bookwurm index: {e.message}')
 			return 1
 
-		res = idx.search(query, {
+		results = idx.search(query, {
 			'showMatchesPosition': True,
 			'attributesToRetrieve': [
-				'title', 'type', 'author', 'keywords', 'file', 'total_pages'
+				'id', 'title', 'type', 'author', 'keywords', 'file', 'total_pages'
 			],
 			'showMatchesPosition': True,
-			'limit': limit
+			'limit': limit,
 		})
 
-		total = res["estimatedTotalHits"]
+		total = results['estimatedTotalHits']
+
 		limited = ''
 		if total > limit:
 			limited = f'Showing {limit} of '
 
-		print(f'{limited}{total} results found in {res["processingTimeMs"]}ms')
+		print(f'{limited}{total} results found in {results["processingTimeMs"]}ms')
 
-		for hit in res['hits']:
-			print(f' [green]*[/] [cyan]{hit["title"]}[/] ([magenta]{len(hit["_matchesPosition"].keys())} matches[/])')
-
-			doc_path = Path(json.loads(hit['file']))
-			doc_meta = f'[red]{hit["type"].upper()}[/], [yellow]{hit["total_pages"]} pages[/]'
-
-			print(f'   [link=file://{doc_path}][blue]{doc_path.stem}[/][/] ({doc_meta})')
+		if detailed:
+			self._print_detailed_results(results, query, limit, idx)
+		else:
+			self._print_simple_results(results, query, limit)
 
 		return 0
